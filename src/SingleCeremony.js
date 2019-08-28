5a43aee5edffef390c0aea2f70185e6083c906ff
Lauren West <laurenwest12@gmail.com>
2019-08-27 21:26:23 -0400

    rendering matchups correctly

--- src/SingleCeremony.js
+++ src/SingleCeremony.js
@@ -9,12 +9,13 @@ const shuffle = array => {
 };
 
 const findRemaining = (arr1, arr2) => {
-  let flat = arr2.reduce((acc, val) => acc.concat(val));
-  const ids = flat.reduce((acc, val) => {
-    acc.push(val.id);
+  const paired = arr2.reduce((acc, val) => {
+    acc.push(val.pair1.id);
+    acc.push(val.pair2.id);
     return acc;
   }, []);
-  return arr1.filter(item => ids.indexOf(item.id) === -1);
+
+  return arr1.filter(item => paired.indexOf(item.id) === -1);
 };
 
 class SingleCeremony extends Component {
@@ -39,7 +40,8 @@ class SingleCeremony extends Component {
   componentDidUpdate(prevProps) {
     if (prevProps !== this.props) {
       const { number } = this.props.match.params;
-      const shuffled = shuffle(this.props.cast);
+      const remaining = findRemaining(this.props.cast, this.state.pairs);
+      const shuffled = shuffle(remaining);
       const current = shuffled[0];
 
       this.setState({
@@ -60,8 +62,7 @@ class SingleCeremony extends Component {
 
   render() {
     const { cast } = this.props;
-
-    console.log(this.state.pairs);
+    const remaining = findRemaining(cast, this.state.pairs);
 
     return (
       <div className="container">
@@ -69,16 +70,19 @@ class SingleCeremony extends Component {
           <label>{this.state.pair1 && this.state.pair1.name}</label>
           <label>{this.state.pair2 && this.state.pair2.name}</label>
           <ul>
-            {cast.map(member => (
-              <li
-                key={member.key}
-                onClick={e => this.handleChange(e, member)}
-                member={member}
-                value={member.id}
-              >
-                {member.name}
-              </li>
-            ))}
+            {remaining.map(
+              member =>
+                member.id !== this.state.pair1.id && (
+                  <li
+                    key={member.key}
+                    onClick={e => this.handleChange(e, member)}
+                    member={member}
+                    value={member.id}
+                  >
+                    {member.name}
+                  </li>
+                )
+            )}
           </ul>
           <button type="submit" className="btn btn-primary">
             Submit
@@ -89,9 +93,9 @@ class SingleCeremony extends Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = ({ cast }) => {
   return {
-    cast: state.cast
+    cast
   };
 };
 
